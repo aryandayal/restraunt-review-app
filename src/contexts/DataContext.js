8a5cc1ce260ae3677842934860d8e81d637e74af
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-07-08 21:15:51 +0530

    fix decimal

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -5,25 +5,29 @@ export const DataContext = createContext();
 
 const DataContextProvider = ({ children }) => {
   const [restaurantsDatas, setRestaurantsDatas] = useState(restaurantsData);
+
   const review = (id, rating, comment) => {
-    restaurantsData = restaurantsData.map((restaurant) => {
-      if (id === restaurant.id) {
-        restaurant.ratings.push({
-          revName: "Ahsan",
-          pp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3uO7UUBnkvtntc2R8Y9krkgWvbl-BTKMazZjg8Ul-gmDgzQeb8I6DIQ&s=0",
-          rating,
-          comment,
-        });
-        restaurant.averageRating = restaurant.ratings.reduce(
-          (acc, { rating }) => acc + rating / restaurant.ratings.length,
-          0
-        );
-        return restaurant;
-      } else {
-        return restaurant;
-      }
-    });
-    setRestaurantsDatas([...restaurantsDatas]);
+    setRestaurantsDatas(
+      restaurantsDatas.map((restaurant) => {
+        if (id === restaurant.id) {
+          restaurant.ratings.push({
+            revName: "Ahsan",
+            pp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3uO7UUBnkvtntc2R8Y9krkgWvbl-BTKMazZjg8Ul-gmDgzQeb8I6DIQ&s=0",
+            rating,
+            comment,
+          });
+          restaurant.averageRating = restaurant.ratings
+            .reduce(
+              (acc, { rating }) => acc + rating / restaurant.ratings.length,
+              0
+            )
+            .toFixed(2);
+          return restaurant;
+        } else {
+          return restaurant;
+        }
+      })
+    );
   };
 
   return (
